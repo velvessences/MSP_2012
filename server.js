a2156d11a2fbc767c63057a09e23fa103f9cf0d8
velvessences <xaviersences@gmail.com>
2026-04-23 14:18:33 +0200

    df

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const PORT = 1600; 
+const PORT = 1600;
 
 const server = http.createServer((req, res) => {
     console.log(`[GAME REQUESTED]: ${req.url}`);
 
-    // If Flash asks for security permission, give it a thumbs up
+    // 1. Give Flash the security pass it needs to talk from GitHub to your computer
     if (req.url.includes('crossdomain.xml')) {
         res.writeHead(200, { 'Content-Type': 'text/xml' });
         res.end(`<?xml version="1.0"?>
@@ -14,14 +14,35 @@ const server = http.createServer((req, res) => {
         return;
     }
 
-    // Default response
+    // 2. Give the game the dummy WSDL XML it is begging for so it stops crashing
+    if (req.url.includes('Service.asmx')) {
+        res.writeHead(200, {
+            'Content-Type': 'text/xml',
+            'Access-Control-Allow-Origin': '*'
+        });
+        res.end(`<?xml version="1.0" encoding="utf-8"?>
+        <wsdl:definitions xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/" xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" targetNamespace="http://tempuri.org/">
+           <wsdl:portType name="ServiceSoap" />
+           <wsdl:binding name="ServiceSoap" type="tns:ServiceSoap">
+              <soap:binding transport="http://schemas.xmlsoap.org/soap/http" />
+           </wsdl:binding>
+           <wsdl:service name="Service">
+              <wsdl:port name="ServiceSoap" binding="tns:ServiceSoap">
+                 <soap:address location="http://localhost:1600/Webservice/Service.asmx" />
+              </wsdl:port>
+           </wsdl:service>
+        </wsdl:definitions>`);
+        return;
+    }
+
+    // Default catch-all
     res.writeHead(200, {
         'Content-Type': 'text/plain',
         'Access-Control-Allow-Origin': '*'
     });
-    res.end('Server is alive!');
+    res.end('OK');
 });
 
 server.listen(PORT, () => {
-    console.log(`Dummy server is listening on http://localhost:${PORT}`);
+    console.log(`Dummy server running on http://localhost:${PORT}`);
 });
